c359dd5194ff3bf602ec2c113292ea6e04e15631
Eugene Rysaj <eugene.rysaj@gmail.com>
2015-03-18 20:12:43 +0200

    add dummy classes conforming to `JSQCollectionViewAdapter` protocol

---
+++ JSQMessagesViewController/Views/JSQCollectionViewAdapterBase.h
@@ -0,0 +1,28 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQMessagesViewController
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQMessagesViewController
+//
+//
+//  License
+//  Copyright (c) 2014 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+#import <Foundation/Foundation.h>
+
+
+/**
+ *  `JSQCollectionViewAdapterBase` is an abstract class conforming to `JSQCollectionViewAdapter` protocol.
+ *  It provides a template implementation and requires certain methods to be overridden by subclass.
+ */
+@interface JSQCollectionViewAdapterBase : NSObject
+
+@end
---
+++ JSQMessagesViewController/Views/JSQMessagesCollectionViewAdapter.h
@@ -0,0 +1,29 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://cocoadocs.org/docsets/JSQMessagesViewController
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQMessagesViewController
+//
+//
+//  License
+//  Copyright (c) 2014 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+#import "JSQCollectionViewAdapterBase.h"
+
+
+/**
+ *  `JSQMessagesCollectionViewAdapter` is a concrete class representing a
+ *  collection view adapter that is designed to display model objects conforming to
+ *  `JSQMessageData` protocol using cells of `JSQMessagesCollectionViewCell` class.
+ */
+@interface JSQMessagesCollectionViewAdapter : JSQCollectionViewAdapterBase
+
+@end
